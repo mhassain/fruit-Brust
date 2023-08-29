c3590ddcc8781073757eb2388b6939afac36507a
mhassain <mahadehasan88@gmail.com>
2023-08-29 15:40:18 +0600

    Fruit Brust

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        baseColor: '#F85559',
+      },
+    },
+  },
+  plugins: [],
+}
+
